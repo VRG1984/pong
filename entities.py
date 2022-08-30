0bb49010e2153e92228b561739eaccd36a56d719
Víctor R <vrg1984@gmail.com>
2022-08-30 23:07:48 +0200

    Aparece en el centro con velocidad cambiada

--- entities.py
+++ entities.py
@@ -15,6 +15,23 @@ class Bola:
     def dibujar(self, pantalla):
         pg.draw.circle(pantalla, self.color, (self.center_x, self.center_y), self.radio)
     
+    def mover(self, x_max = 800, y_max = 600):
+        self.center_x += self.vx
+        self.center_y += self.vy
+
+        if self.center_y >= y_max -self.radio or self.center_y < self.radio:
+            self.vy = self.vy * -1
+        
+        if self.center_x >= x_max or self.center_x <= 0: #Se cuela por la dere
+            self.center_x = x_max // 2
+            self.center_y = y_max // 2
+
+            #Hacer un random en el eje y para que la bola salga con distinta inclinación
+            self.vx *= -1
+            self.vy *= -1
+        
+
+    
 class Raqueta:
     def __init__(self, center_x, center_y, w=120, h=20, color=(255, 255, 0)):
         self.center_x = center_x
@@ -29,5 +46,16 @@ class Raqueta:
     def dibujar(self, pantalla):
         pg.draw.rect(pantalla, self.color, (self.center_x - self.w//2, self.center_y - self.h//2, self.w, self.h))
 
+    def mover(self, tecla_arriba, tecla_abajo, y_max=600):
+        estado_teclas = pg.key.get_pressed()
+        if estado_teclas[tecla_arriba]: #Movimiento hacia arriba
+            self.center_y -= self.vy
+        if self.center_y < self.h // 2:
+            self.center_y = self.h // 2
+        
+        if estado_teclas[tecla_abajo]: # Movimiento hacia abajo
+            self.center_y += self.vy
+        if self.center_y > y_max - self.h // 2:
+            self.center_y = y_max - self.h // 2
 
     
--- main.py
+++ main.py
@@ -5,15 +5,21 @@ pg.init()
 
 pantalla_principal = pg.display.set_mode((800, 600))
 pg.display.set_caption("Pong")
+cronometro = pg.time.Clock()
 
 game_over = False
 
 bola = Bola(400, 300, color=(255, 255, 255))
+bola.vx = 4
+bola.vy = -4
 raqueta1 = Raqueta(20, 300, w=20, h=120)
 raqueta2 = Raqueta(780, 300, w=20, h=120)
-raqueta2.vy = 5
+raqueta2.vy = 3
+raqueta1.vy = 3
 
 while not game_over:
+    dt = cronometro.tick(60)
+    #1000 milisegundos/60 fps = 16 ms entre un fotograma y otro
     #Eventos que hace el usuario y devuelve una lista de eventos
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
@@ -25,12 +31,12 @@ while not game_over:
                 raqueta2.center_y -= raqueta2.vy"""
     
     # El get_pressed lo que hace es devolver una lista con todas las teclas donde le dice con false o true qué teclas se han pulsado
+    
+    """Tarea"""
     # Meter esto como método "actualizar" en Raqueta
-    estado_teclas = pg.key.get_pressed()
-    if estado_teclas[pg.K_UP]:
-        raqueta2.center_y -= raqueta2.vy
-    if estado_teclas[pg.K_DOWN]:
-        raqueta2.center_y += raqueta2.vy
+    raqueta2.mover(pg.K_UP, pg.K_DOWN)
+    raqueta1.mover(pg.K_a, pg.K_z)
+    bola.mover()
 
     pantalla_principal.fill((0, 0, 0))
     #Le pasa la info a la tarjeta gráfica y lo saca por pantalla
